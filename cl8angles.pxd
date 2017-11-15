c60ce85fc95a83b3d41256c967757ca98927c094
Philip Graae <mrpgraae@gmail.com>
2017-11-15 17:32:40 +0100

    Finished api function

--- cl8angles.pxd
+++ cl8angles.pxd
@@ -1,5 +1,5 @@
 cdef extern from ias_lib/ias_angle_gen_distro.h:
-    ctypedef struct IAS_ANGLE_GEN_METADATA:
+    cdef struct IAS_ANGLE_GEN_METADATA:
         pass
 
     int ias_angle_gen_read_ang(const char* ang_filename,
@@ -15,18 +15,27 @@ cdef extern from ias_lib/ias_satellite_attributes.h:
     int ias_sat_attr_initialize(IAS_SATELLITE_ID satellite_id)
 
 cdef extern from l8_angles.h:
-    ctypedef enum ANGLE_TYPE:
+    cdef enum angle_type:
         AT_UNKNOWN = 0,
         AT_BOTH,
         AT_SATELLITE,
         AT_SOLAR
+    ctypedef angle_type ANGLE_TYPE
+
+    cdef struct angles_frame:
+        int num_lines
+        int num_samps
+    ctypedef angles_frame ANGLES_FRAME
     ctypedef struct L8_ANGLES_PARAMETERS:
         pass
 
-    int calculate_angles(const IAS_ANGLE_GEN_METADATA* metadata, int line, 
-                         int samp, int band_index, ANGLE_TYPE angle_type,
-                         double* sat_angles, double* sun_angles)
+    int l8_angles(int band_idx, int n_lines, int n_samples,
+                  int sub_sample, ANGLE_TYPE angle,
+                  const IAS_ANGLE_GEN_METADATA* metadata,
+                  double* sun_az, double* sun_zn,
+                  double* sat_az, double* sat_zn)
 
-cdef extern from l8_angles.c:
-    int process_parameters(const int argument_count, char** arguments,
-                           L8_ANGLES_PARAMETERS* parameters)
+cdef extern from angles_api.c:
+    int get_frame(const IAS_ANGLE_GEN_METADATA* metadata,
+                  int band_index, ANGLES_FRAME* frame)
+
--- l8angles.pyx
+++ l8angles.pyx
@@ -30,12 +30,15 @@ def init_bandlist(bands):
 def calculate_angles(metadata_fp, angle_type='BOTH',
                      int subsample=1, bands=None):
 
+    cdef int status = 0
+
     if bands is None:
-        bandlist = np.ones(11, dtype=np.intc)
+        bandlist = np.arange(11, dtype=np.intc)
     else:
         if any(x < 1 for x in bandlist) or any(x > 11 for x in bandlist):
             raise ValueError('Invalid bandindex in bandlist')
-        bandlist = init_bandlist(bands)
+        bandlist = np.array(bands, dtype=np.intc)
+        bandlist -= 1
 
     if subsample < 1:
         raise ValueError('Subsample must be a positive integer')
@@ -59,9 +62,60 @@ def calculate_angles(metadata_fp, angle_type='BOTH',
     cdef char* c_filename = filename
 
     cdef IAS_ANGLE_GEN_METADATA metadata
-    ias_angle_gen_read_ang(c_filename, &metadata)
+    status = cl8angles.ias_angle_gen_read_ang(c_filename, &metadata)
+    if status == -1:
+        raise ValueError('Invalid metadata file')
 
+    if angle_type == 'BOTH':
+        data = {'sun_az': [], 'sun_zn': [], 'sat_az': [], 'sat_zn': []}
+    if angle_type == 'SATELLITE':
+        data = {'sat_az': [], 'sat_zn': []}
+    if angle_type == 'SOLAR':
+        data = {'sun_az': [], 'sun_zn': []}
+    
+    cdef int n_lines
+    cdef int n_samps
+    cdef ANGLES_FRAME frame
+    for i in range(bandlist.size):
+        band = bandlist[i]
+        status = cl8angles.get_frame(&metadata, band, &frame)
+        if status == -1:
+            raise ValueError('Band {} not present in metadata file'.format(band + 1))
+        n_lines = (frame.num_lines - 1) // subsample + 1
+        n_samps = (frame.num_samps - 1) // subsample + 1
+        if angle_type == 'BOTH': 
+            sun_az = np.empty((n_lines, n_samps), dtype=np.float64)
+            sun_zn = np.empty((n_lines, n_samps), dtype=np.float64)
+            sat_az = np.empty((n_lines, n_samps), dtype=np.float64)
+            sat_zn = np.empty((n_lines, n_samps), dtype=np.float64)
+            status = cl8angles.l8_angles(band, n_lines, n_samps, subsample, ang, &metadata,
+                                         &sun_az[0,0], &sun_zn[0,0], &sat_az[0,0], &sat_zn[0,0])
+            if status == -1:
+                raise RuntimeError('Something horrible happened')
+            data['sun_az'].append(sun_az)
+            data['sun_zn'].append(sun_zn)
+            data['sat_az'].append(sat_az)
+            data['sat_zn'].append(sat_zn)
+        if angle_type == 'SATELLITE': 
+            sat_az = np.empty((n_lines, n_samps), dtype=np.float64)
+            sat_zn = np.empty((n_lines, n_samps), dtype=np.float64)
+            status = cl8angles.l8_angles(band, n_lines, n_samps, subsample, ang, &metadata,
+                                         NULL, NULL, &sat_az[0,0], &sat_zn[0,0])
+            if status == -1:
+                raise RuntimeError('Something horrible happened')
+            data['sat_az'].append(sat_az)
+            data['sat_zn'].append(sat_zn)
+        if angle_type == 'SOLAR': 
+            sun_az = np.empty((n_lines, n_samps), dtype=np.float64)
+            sun_zn = np.empty((n_lines, n_samps), dtype=np.float64)
+            status = cl8angles.l8_angles(band, n_lines, n_samps, subsample, ang, &metadata,
+                                         &sun_az[0,0], &sun_zn[0,0], NULL, NULL)
+            if status == -1:
+                raise RuntimeError('Something horrible happened')
+            data['sat_az'].append(sat_az)
+            data['sat_zn'].append(sat_zn)
 
+    return data
 
 
 cdef _calculate_angles(char** argv, int sample_factor, ANGLE_TYPE angle_type):

d6850ecf84470a002ab4fcc0c184830120c55ab1
Philip Graae <mrpgraae@gmail.com>
2017-11-16 11:49:50 +0100

    Added bat file for setting up env vars

---
+++ setvars.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+set INCLUDE=%INCLUDE%C:\Users\phgr\Documents\l8_angles\src\ias_lib;
+set LIB=%LIB%C:\Users\phgr\Documents\l8_angles\src\ias_lib;
+cd C:\Users\phgr\Documents\l8_angles

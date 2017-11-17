dfb7a0e84224454b75cbcd4f795e68afcb85ed3c
Philip Graae <mrpgraae@gmail.com>
2017-11-17 14:13:23 +0100

    Proper relative paths

--- setvars.bat
+++ setvars.bat
@@ -1,4 +1,5 @@
+set WORKDIR=%~dp0
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-set INCLUDE=%INCLUDE%C:\Users\phgr\Documents\l8_angles\src\ias_lib;
-set LIB=%LIB%C:\Users\phgr\Documents\l8_angles\src\ias_lib;
-cd C:\Users\phgr\Documents\l8_angles
+cd %WORKDIR%
+set INCLUDE=%INCLUDE%%WORKDIR%src\ias_lib;
+set LIB=%LIB%%WORKDIR%src\ias_lib;

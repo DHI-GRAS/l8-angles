b66bbe9d36da16dc49ae9476a0240fefcf7ed86e
Philip Graae <mrpgraae@gmail.com>
2017-11-20 14:23:45 +0100

    Move build script to recipe repo

--- setvars.bat
+++
@@ -1,5 +0,0 @@
-set WORKDIR=%~dp0
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-cd %WORKDIR%
-set INCLUDE=%INCLUDE%%WORKDIR%src\ias_lib;
-set LIB=%LIB%%WORKDIR%src\ias_lib;
